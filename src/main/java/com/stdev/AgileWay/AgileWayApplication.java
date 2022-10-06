2fce1efd40cd5e922312ac0abe304cdbc07650d9
BOURHOU Abdelilah <abdou.loujdi7@gmail.com>
2022-10-06 21:29:10 +0100

    initial commit

---
+++ src/main/java/com/stdev/AgileWay/AgileWayApplication.java
@@ -0,0 +1,13 @@
+package com.stdev.AgileWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stdev/AgileWay/AgileWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stdev.AgileWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
